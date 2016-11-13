2ec76bfdd9b750e354e9e1a52d6f21c7371241dd
Joey Kirby <jkirbymusic@gmail.com>
2016-11-12 20:27:40 -0600

    Disable attack BTN @ 0/ Alert Player X wins! TODO-Update DOM health

--- javascripts/app.js
+++ javascripts/app.js
@@ -8,7 +8,7 @@ var Gauntlet = (function(Gauntlet) {
   // warrior.generateClass(); // This will be used for "Surprise me" option
   // console.log(warrior.toString());
 
-//---------------- BAD GUY --------------------//
+  //---------------- BAD GUY --------------------//
   var orc = new Gauntlet.Combatants.Orc();
   orc.generateClass();
   orc.setWeapon(new BroadSword());
@@ -18,76 +18,76 @@ var Gauntlet = (function(Gauntlet) {
     Test code to generate a spell
    */
 
-//---------------CHARACTER OBJECT----------------//
+  //---------------CHARACTER OBJECT----------------//
   var newPlay = new Gauntlet.Combatants.Human();
 
-//-------------------------- ADD CLASS -----------------------------//
-  $(".classRow").click( function(event) {
+  //-------------------------- ADD CLASS -----------------------------//
+  $(".classRow").click(function(event) {
     var idValue = event.target.id;
     // newPlay.setClass(idValue);
     // console.log("id", idValue);
-   switch (idValue) {
+    switch (idValue) {
       case "Warrior":
-      newPlay.class = new Gauntlet.GuildHall.Warrior();
-      console.log("newPlay-Case: Warrior", newPlay);
-      // newPlay.class = Gauntlet.GuildHall.allowedClasses[0];
+        newPlay.class = new Gauntlet.GuildHall.Warrior();
+        console.log("newPlay-Case: Warrior", newPlay);
+        // newPlay.class = Gauntlet.GuildHall.allowedClasses[0];
         break;
       case "Wizard":
-      newPlay.class = new Gauntlet.GuildHall.Wizard();
-      console.log("newPlay-Case: Wizard", newPlay);
+        newPlay.class = new Gauntlet.GuildHall.Wizard();
+        console.log("newPlay-Case: Wizard", newPlay);
         break;
       case "Thief":
-      newPlay.class = new Gauntlet.GuildHall.Thief();
-      console.log("newPlay-Case: Thief", newPlay);
-      // newPlay.class = Gauntlet.GuildHall.allowedClasses[Warrior];
+        newPlay.class = new Gauntlet.GuildHall.Thief();
+        console.log("newPlay-Case: Thief", newPlay);
+        // newPlay.class = Gauntlet.GuildHall.allowedClasses[Warrior];
         break;
       case "surprise-me-class":
-      newPlay.generateClass();
-      console.log("newPlay-Case: Random Class", newPlay);
-      // newPlay.class = Gauntlet.GuildHall.allowedClasses[Warrior];
+        newPlay.generateClass();
+        console.log("newPlay-Case: Random Class", newPlay);
+        // newPlay.class = Gauntlet.GuildHall.allowedClasses[Warrior];
         break;
     };
   });
 
-//------------------------------ ADD WEAPON ----------------------------------//
-  $(".weaponDiv").click( function(event) {
+  //------------------------------ ADD WEAPON ----------------------------------//
+  $(".weaponDiv").click(function(event) {
     var idValue = event.target.id;
     // newPlay.setWeapon(idValue);
-     switch (idValue) {
+    switch (idValue) {
       case "Dagger":
-      newPlay.weapon = new Dagger();
-      console.log("newPlay-Case: Dagger", newPlay);
-      // newPlay.setWeapon(new Dagger());
+        newPlay.weapon = new Dagger();
+        console.log("newPlay-Case: Dagger", newPlay);
+        // newPlay.setWeapon(new Dagger());
         break;
       case "Broad Sword":
-      newPlay.weapon = new BroadSword();
-      console.log("newPlay-Case: BSword", newPlay);
-      // newPlay.setWeapon(new BroadSword());
+        newPlay.weapon = new BroadSword();
+        console.log("newPlay-Case: BSword", newPlay);
+        // newPlay.setWeapon(new BroadSword());
         break;
       case "War-Axe":
-      newPlay.weapon = new WarAxe();
-      console.log("newPlay-Case: War-Axe", newPlay);
+        newPlay.weapon = new WarAxe();
+        console.log("newPlay-Case: War-Axe", newPlay);
         break;
       case "surprise-me-weapon":
-      newPlay.setWeapon(new BroadSword()); //---------------------------> Need to create generateWeapon(); 
-      console.log("newPlay-Case: Random Weapon", newPlay); 
-      // newPlay.class = Gauntlet.GuildHall.allowedClasses[Warrior];
+        newPlay.setWeapon(new BroadSword()); //---------------------------> Need to create generateWeapon(); 
+        console.log("newPlay-Case: Random Weapon", newPlay);
+        // newPlay.class = Gauntlet.GuildHall.allowedClasses[Warrior];
         break;
     };
   });
 
-//------------------------------- SPELL STUFF ------------------------------------//
+  //------------------------------- SPELL STUFF ------------------------------------//
 
   var spell = new Gauntlet.SpellBook.Sphere();
   // console.log("spell: ", spell.toString());
   // console.log("Gauntlet: ", Gauntlet);
 
-    var spell = new Gauntlet.SpellBook.Sphere();
-    // console.log("spell: ", spell.toString());
-    // console.log("Gauntlet: ", Gauntlet);
+  var spell = new Gauntlet.SpellBook.Sphere();
+  // console.log("spell: ", spell.toString());
+  // console.log("Gauntlet: ", Gauntlet);
 
 
-//------------------------------ SHOW PAGES - CLICK TO NEXT PAGE -----------------------------//
+  //------------------------------ SHOW PAGES - CLICK TO NEXT PAGE -----------------------------//
   $(document).ready(function() {
     /*
       Show the initial view that accepts player name
@@ -105,7 +105,7 @@ var Gauntlet = (function(Gauntlet) {
 
       switch (nextCard) {
         case "card--class":
-        newPlay.playerName = $("#checkout").val();
+          newPlay.playerName = $("#checkout").val();
           moveAlong = ($("#player-name").val() !== "");
           break;
         case "card--weapon":
@@ -113,7 +113,7 @@ var Gauntlet = (function(Gauntlet) {
           break;
         case "card--battleground":
           moveAlong = ($("#player-name").val() !== "");
-          ////////////////////////////Adding player Card to DOM///////////////////////////////////////////////////////////
+          //----------------------------------------- Adding player Card to DOM -------------------------------------//
           $('.playercard').append(`<div class="card-block">
                                       <h4 class="card-title">${newPlay.playerName}</h4>
                                       <h5 class="card-subtitle text-muted">Class: ${newPlay.class}</h5>
@@ -121,8 +121,8 @@ var Gauntlet = (function(Gauntlet) {
                                       <p class="card-text">Weapon Damage: ${newPlay.weapon.damage}</p>
                                       <p class="card-text">Health: ${newPlay.health}</p>
                                       </div> <img src="/images/Ninja.png" alt="Card image">`)
-////////////////////////////Adding Enemy Card to DOM///////////////////////////////////////////////////////////
-          
+
+            //----------------------------------- Adding Enemy Card to DOM ------------------------------------//
           $('.playercard2').append(`<div class="card-block">
                                       <h4 class="card-title">${orc.playerName}</h4>
                                       <h5 class="card-subtitle text-muted">Class: ${orc.class}</h5>
@@ -130,18 +130,23 @@ var Gauntlet = (function(Gauntlet) {
                                       <p class="card-text">Weapon Damage: ${orc.weapon.damage}</p>
                                       <p class="card-text">Health: ${orc.health}</p>
                                       </div> <img src="/images/Ninja-Free-Download-PNG.png" alt="Card image">`)
-/////////////////////////Function for Deducting Health////////////////////////////////////////////////////          
-          $('#play_button').click(function(){
+
+            //------------------------------ Function for Deducting Health ---------------------------//          
+          $('#play_button').click(function() {
             console.log("something")
-            if(event){
-           newPlay.health = (newPlay.health)-(orc.weapon.damage)
-           orc.health = (orc.health)-(newPlay.weapon.damage)
-           console.log("Player:" + newPlay.health , "Orc:" + orc.health)
-            if(newPlay.health===0 || orc.health === 0){
-              $('#play_button').unbind()
+            if (event) {
+              newPlay.health = (newPlay.health) - (orc.weapon.damage)
+              orc.health = (orc.health) - (newPlay.weapon.damage)
+              console.log("Player:" + newPlay.health, "Orc:" + orc.health)
+              if (newPlay.health <= 0) {
+                $('#play_button').unbind().disabled = true;
+                alert("KO! ORC WINS!");
+              } else if (orc.health <= 0) {
+                $('#play_button').unbind().disabled = true;
+                alert("KO! " + newPlay.playerName + " WINS!");
+              }
             }
-            }
-            
+
           })
           break;
       }
